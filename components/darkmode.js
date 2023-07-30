0d369cb3059ec3b3ef4f78c4a86b8eb8ec23c60a
mohamed saied <127026703+mohamedsaied22@users.noreply.github.com>
2023-07-30 15:59:15 +0300

    update dark mode

--- components/darkmode.js
+++ components/darkmode.js
@@ -1,3 +1,63 @@
+// import Image from "next/image";
+// import { useState, useEffect } from "react";
+// import { useTheme } from "next-themes";
+
+// //icons
+// import { FaMoon } from "react-icons/fa";
+// import { BsLightbulb } from "react-icons/bs";
+
+// export default function DarkModeButton() {
+//   const { systemTheme, theme, setTheme } = useTheme();
+//   const [mounted, setMounted] = useState(false);
+
+//   useEffect(() => {
+//     setMounted(true);
+//   }, []);
+
+//   if (!mounted) return null;
+//   const currentTheme = theme === "system" ? systemTheme : theme;
+
+//   const toggleTheme = () => {
+//     if (currentTheme === "dark") {
+//       setTheme("light");
+//       document.documentElement.classList.remove("dark");
+//     } else {
+//       setTheme("dark");
+//       document.documentElement.classList.add("dark");
+//     }
+//   };
+
+
+
+//   return (
+//     <div className="">
+//       <div>
+//           {/* {currentTheme === "dark" ? "Dark" : "Light"}{" "} */}
+    
+//         <div >
+//           {currentTheme === "dark" ? (
+//             <button
+//               className="bg-white/70 hover:bg-white rounded-[50%] p-[12px] shadow-2xl "
+//               onClick={toggleTheme}
+//             >
+//               <BsLightbulb className="text-black" />
+//             </button>
+//           ) : (
+//             <button
+//               className="bg-black/70 hover:bg-black rounded-[40%]  border-2 p-[12px] shadow-2xl"
+//               onClick={toggleTheme}
+//             >
+//               <FaMoon className="text-white" />
+//             </button>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
@@ -12,6 +72,18 @@ export default function DarkModeButton() {
 
   useEffect(() => {
     setMounted(true);
+    // Check if there is a saved preference for the user's theme
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setTheme(savedTheme);
+      if (savedTheme === "dark") {
+        document.documentElement.classList.add("dark");
+      }
+    } else {
+      // If there is no saved preference, set the default theme to dark
+      setTheme("dark");
+      document.documentElement.classList.add("dark");
+    }
   }, []);
 
   if (!mounted) return null;
@@ -21,9 +93,11 @@ export default function DarkModeButton() {
     if (currentTheme === "dark") {
       setTheme("light");
       document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     } else {
       setTheme("dark");
       document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     }
   };
 
