e1bf6d8d319b26058f2e22b0988de7e74a2e04e3
mohamedsaied22 <mohamedsaid2994@gmail.com>
2024-07-14 09:54:28 +0300

    update dark mode

--- components/darkmode.js
+++ components/darkmode.js
@@ -1,63 +1,3 @@
-// import Image from "next/image";
-// import { useState, useEffect } from "react";
-// import { useTheme } from "next-themes";
-
-// //icons
-// import { FaMoon } from "react-icons/fa";
-// import { BsLightbulb } from "react-icons/bs";
-
-// export default function DarkModeButton() {
-//   const { systemTheme, theme, setTheme } = useTheme();
-//   const [mounted, setMounted] = useState(false);
-
-//   useEffect(() => {
-//     setMounted(true);
-//   }, []);
-
-//   if (!mounted) return null;
-//   const currentTheme = theme === "system" ? systemTheme : theme;
-
-//   const toggleTheme = () => {
-//     if (currentTheme === "dark") {
-//       setTheme("light");
-//       document.documentElement.classList.remove("dark");
-//     } else {
-//       setTheme("dark");
-//       document.documentElement.classList.add("dark");
-//     }
-//   };
-
-
-
-//   return (
-//     <div className="">
-//       <div>
-//           {/* {currentTheme === "dark" ? "Dark" : "Light"}{" "} */}
-    
-//         <div >
-//           {currentTheme === "dark" ? (
-//             <button
-//               className="bg-white/70 hover:bg-white rounded-[50%] p-[12px] shadow-2xl "
-//               onClick={toggleTheme}
-//             >
-//               <BsLightbulb className="text-black" />
-//             </button>
-//           ) : (
-//             <button
-//               className="bg-black/70 hover:bg-black rounded-[40%]  border-2 p-[12px] shadow-2xl"
-//               onClick={toggleTheme}
-//             >
-//               <FaMoon className="text-white" />
-//             </button>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-
-
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
@@ -84,7 +24,7 @@ export default function DarkModeButton() {
       setTheme("dark");
       document.documentElement.classList.add("dark");
     }
-  }, []);
+  }, [setTheme]);
 
   if (!mounted) return null;
   const currentTheme = theme === "system" ? systemTheme : theme;
